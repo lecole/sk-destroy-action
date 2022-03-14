8682bd7dec96dc32496f40bbd655b09c74003e54
Lecole <lecole@gmail.com>
2022-03-14 18:31:37 +0700

    Check env

--- main.py
+++ main.py
@@ -14,16 +14,19 @@ def main():
     api_username = '__token__'
     api_key = os.environ["INPUT_SKYAPIKEY"]
 
+    for k, v in os.environ.items():
+        print(k, v)
+
     data = {
         'git_intg_access_id': os.environ["INPUT_SKYGITINTGACCESSID"],
         'git_repo_slug': os.environ["INPUT_SKYGITREPOSLUG"],
 
-        'github_base_ref': os.environ["GITHUB_BASE_REF"],
-        'github_pull_request_number': os.environ["GITHUB_REF"].split('/')[2],
-        'github_actor': os.environ["GITHUB_ACTOR"],
-        'github_event_name': os.environ["GITHUB_EVENT_NAME"],
-        'github_head_ref': os.environ["GITHUB_HEAD_REF"],
-        'gethub_repo_owner': os.environ["GITHUB_REPOSITORY_OWNER"],
+        # 'github_base_ref': os.environ["GITHUB_BASE_REF"],
+        # 'github_pull_request_number': os.environ["GITHUB_REF"].split('/')[2],
+        # 'github_actor': os.environ["GITHUB_ACTOR"],
+        # 'github_event_name': os.environ["GITHUB_EVENT_NAME"],
+        # 'github_head_ref': os.environ["GITHUB_HEAD_REF"],
+        # 'gethub_repo_owner': os.environ["GITHUB_REPOSITORY_OWNER"],
     }
 
     query_data = requests.post(
